a5f42821daab33d911de529b063625d67fd90c1d
KENNETH MASING <kenneth_masing@yahoo.com>
2020-05-26 23:59:20 +0800

    operation arguments

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { GraphQLServer } from 'graphql-yoga'
 // TYPE DEFINITIONS (APPLICATION SCHEMA)
 const typeDefs = `
     type Query {
+        greeting(name: String, position: String): String!
+        add(a: Float!, b: Float!): Float!
         me: User!
         post: Post!
     }
@@ -27,6 +29,16 @@ const typeDefs = `
 // RESOLVERS
 const resolvers = {
     Query: {
+        greeting(parent, args, ctx, info) {
+            if(args.name && args.position) {
+                return `Hello, ${args.name}! You are my favorite ${args.position}.`
+            } else {
+                return `Hello!`
+            }
+        },
+        add(parent, args, ctx, info) {
+            return args.a + args.b
+        },
         me() {
             return {
                 _id: '123098',
